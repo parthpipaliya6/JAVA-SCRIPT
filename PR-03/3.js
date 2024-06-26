19fcd64dd5a8910a584604aad8b298a92832a9f3
Parth Pipaliya <158568547+parthpipaliya6@users.noreply.github.com>
2024-06-26 11:26:49 +0530

    26-06-24

---
+++ PR-03/3.js
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = 10;
+
+if (num1 > num2) {
+  console.log(num1);
+} else if (num1 < num2) {
+  console.log(num2);
+} else {
+  console.log("num1 and num2 are equal");
+}
---
+++ PR-03/7.js
@@ -0,0 +1,7 @@
+let a=6;
+if (a%3==0) {
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
