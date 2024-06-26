8b959b7897e41abb6c3b24f9ecdf5d10a42a09a8
Parth Pipaliya <158568547+parthpipaliya6@users.noreply.github.com>
2024-06-26 11:52:27 +0530

    Java

---
+++ PR-02/3.js
@@ -0,0 +1,14 @@
+// Create variables and assign values
+const one = 10;
+const two = 15;
+const three = 8;
+const four = 12;
+const five = 5;
+
+// Calculate the sum
+const totalSum = one + two + three + four + five;
+
+// Double the sum
+const result = totalSum * 2;
+
+console.log("Result:", result);
