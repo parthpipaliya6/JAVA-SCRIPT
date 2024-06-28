f4bc74266ba659bf8c38149fa35adbbb70196e26
Parth Pipaliya <158568547+parthpipaliya6@users.noreply.github.com>
2024-06-28 17:20:48 +0530

    Java

---
+++ PR-05/6/6.js
@@ -0,0 +1,7 @@
+const print = () => {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+  
+    document.getElementById("output1").innerHTML = `${a}`;
+    document.getElementById("output2").innerHTML = `${b}`;
+  }
