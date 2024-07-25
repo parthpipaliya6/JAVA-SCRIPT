0a7a6c8f8a62ea3bd8a8dc608d4b53892e2ed86f
parthpipaliya6 <shardabenpipaliya5@gmail.com>
2024-07-25 21:33:09 +0530

    java script

--- JS-EXAM-4/index.js
+++ JS-EXAM-4/index.js
@@ -6,7 +6,54 @@ const handledelete = (index) => {
     content()
 };
 
+const handleSalesData = (course, count, fee) => {
+
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    td1.innerHTML = course
+    td2.innerHTML = count
+    td3.innerHTML = fee
+    tr.append(td1, td2, td3);
+    document.getElementById("tbody2").append(tr)
+
+}
 const content = () => {
+    let sales = {
+        FullStack: {
+            count: 0,
+            fee: 0
+        },
+        BackendDevelopment: {
+            count: 0,
+            fee: 0
+        },
+        FrontendDevelopment: {
+            count: 0,
+            fee: 0
+        },
+        UI_UXDesigner: {
+            count: 0,
+            fee: 0
+        },
+        Python: {
+            count: 0,
+            fee: 0
+        },
+        Flutter: {
+            count: 0,
+            fee: 0
+        },
+        AR_VR: {
+            count: 0,
+            fee: 0
+        },
+        Ai_Ml: {
+            count: 0,
+            fee: 0
+        },
+    }
     document.getElementById("tbody").innerHTML = "";
     for (let i = 0; i < users.length; i++) {
         let tr = document.createElement("tr");
@@ -25,40 +72,34 @@ const content = () => {
         let td6 = document.createElement("td");
         td6.innerHTML = "remove";
         td6.addEventListener("click", handledelete);
-
-        tr.append(td0, td1, td2, td3, td4, td5, td6);
         document.getElementById("tbody").append(tr);
+        tr.append(td0, td1, td2, td3, td4, td5, td6);
+        console.log("users[i].course", users[i].course);
+
+        
+        sales[users[i].course].fee += users[i].fee;
+        sales[users[i].course].count += 1
+
     }
+    document.getElementById("tbody2").innerHTML = ""
+
+    handleSalesData("FullStack", sales.FullStack.count, sales.FullStack.fee)
+    handleSalesData("BackendDevelopment", sales.BackendDevelopment.count, sales.BackendDevelopment.fee)
+    handleSalesData("FrontendDevelopment", sales.FrontendDevelopment.count, sales.FrontendDevelopment.fee)
+    handleSalesData("UI_UXDesigner", sales.UI_UXDesigner.count, sales.UI_UXDesigner.fee)
+    handleSalesData("Python", sales.Python.count, sales.Python.fee)
+    handleSalesData("Flutter", sales.Flutter.count, sales.Flutter.fee)
+    handleSalesData("AR_VR", sales.AR_VR.count, sales.AR_VR.fee)
+    handleSalesData("Ai_Ml", sales.Ai_Ml.count, sales.Ai_Ml.fee)
 };
-// const content2 = () => {
-//     let deptcount = 0;
-//     for (let i = 0; i < users.length; i++) {
-//         if (users[i].course === users[i].totalfees) {
-//             deptcount++;
-//         }
-//     }
 
-//     document.getElementById("tbody2").innerHTML = "";
-//     let tr = document.createElement("tr");
-//     let td1 = document.createElement("td");
-//     td1.innerHTML = users[i].course;
-//     let td2 = document.createElement("td");
-//     td2.innerHTML = deptcount;
-
-//     let td3 = document.createElement("td")
-//     td3.innerHTML = users[i].count;
-//     let td4 = document.createElement("td");
-//     td4.innerHTML = users[i].totalfees;
-//     document.getElementById("tbody2").append(tr);
-//     tr.append(td1, td2, td3, td4);
-// };
 const handleData = (e) => {
     e.preventDefault();
     let user = {
         studentName: document.getElementById("studentName").value,
         grid: document.getElementById("Grid").value,
         course: document.getElementById("course").value,
-        fee: document.getElementById("fee").value,
+        fee: Number(document.getElementById("fee").value),
         number: document.getElementById("number").value
     }
 
@@ -74,14 +115,13 @@ const handleData = (e) => {
     }
 
     if (user.number.length != 10 || !['6', '7', '8', '9'].includes(user.number[0])) {
-        alert('Number should be at least 10 digits long & number start is 8 9 7 6');
+        alert('"Phone number should be at least 10 digits long and start with 6 or 7 8 or 9"');
         return;
     }
 
     users.push(user);
     console.log(user);
     content();
-    content2();
 };
 
 const deleteUser = (index) => {
